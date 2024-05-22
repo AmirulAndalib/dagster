000a416fe9f09b0e421594236ed5fca246e1a912
JamieDeMaria <jamie@dagsterlabs.com>
2024-05-22 15:33:38 -0400

    able to fetch logs together in gql

--- js_modules/dagster-ui/packages/ui-core/src/graphql/types.ts
+++ js_modules/dagster-ui/packages/ui-core/src/graphql/types.ts
@@ -3047,6 +3047,7 @@ export type PartitionBackfill = {
   id: Scalars['String']['output'];
   isAssetBackfill: Scalars['Boolean']['output'];
   isValidSerialization: Scalars['Boolean']['output'];
+  logEvents: InstigationEventConnection;
   numCancelable: Scalars['Int']['output'];
   numPartitions: Maybe<Scalars['Int']['output']>;
   partitionNames: Maybe<Array<Scalars['String']['output']>>;
@@ -10604,6 +10605,12 @@ export const buildPartitionBackfill = (
       overrides && overrides.hasOwnProperty('isValidSerialization')
         ? overrides.isValidSerialization!
         : false,
+    logEvents:
+      overrides && overrides.hasOwnProperty('logEvents')
+        ? overrides.logEvents!
+        : relationshipsToOmit.has('InstigationEventConnection')
+        ? ({} as InstigationEventConnection)
+        : buildInstigationEventConnection({}, relationshipsToOmit),
     numCancelable:
       overrides && overrides.hasOwnProperty('numCancelable') ? overrides.numCancelable! : 53,
     numPartitions:
