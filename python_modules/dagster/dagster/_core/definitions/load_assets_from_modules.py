f1c1626a01d6606958bb5d7671dfaaa3ccaf5558
Sandy Ryza <sandy@elementl.com>
2024-05-29 16:52:35 -0700

    remove check props from AssetsDefinition.with_attributes

--- python_modules/dagster/dagster/_core/definitions/load_assets_from_modules.py
+++ python_modules/dagster/dagster/_core/definitions/load_assets_from_modules.py
@@ -420,21 +420,11 @@ def prefix_assets(
                 input_asset_key_replacements[dep_asset_key] = AssetKey(
                     [*source_key_prefix, *dep_asset_key.path]
                 )
-        check_specs_by_output_name = {
-            output_name: check_spec.with_asset_key_prefix(key_prefix)
-            for output_name, check_spec in assets_def.check_specs_by_output_name.items()
-        }
-
-        selected_asset_check_keys = {
-            key.with_asset_key_prefix(key_prefix) for key in assets_def.check_keys
-        }
 
         result_assets.append(
             assets_def.with_attributes(
                 output_asset_key_replacements=output_asset_key_replacements,
                 input_asset_key_replacements=input_asset_key_replacements,
-                check_specs_by_output_name=check_specs_by_output_name,
-                selected_asset_check_keys=selected_asset_check_keys,
             )
         )
 
@@ -478,7 +468,7 @@ def assets_with_attributes(
                 group_names_by_key=(
                     {asset_key: group_name for asset_key in asset.keys}
                     if group_name is not None
-                    else None
+                    else {}
                 ),
                 freshness_policy=freshness_policy,
                 auto_materialize_policy=auto_materialize_policy,
